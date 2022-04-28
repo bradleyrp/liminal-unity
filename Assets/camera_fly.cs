d546d6976fc91ee421fba87b0c69e8b03e9ddd83
Ryan Bradley <bradleyrp@gmail.com>
2022-04-28 09:47:33 -0400

    added readme and cleaned up some code

--- Assets/camera_fly.cs
+++ Assets/camera_fly.cs
@@ -20,10 +20,9 @@ public class camera_fly : MonoBehaviour {
     float mainSpeed = 10.0f; // alternative speed
     float altSpeed = 100.0f; // alternative speed
     // we remove shiftAdd and maxShift from 250 and 1000 to go slower instead
-    //! float shiftAdd = 250.0f; // faster motion, scaled by duration of shift
-    //! float maxShift = 1000.0f; //Maximum speed when holding shift
-    //! float camSens = 0.25f; // how sensitive it with mouse
-    //! float camSens = 0.25f; // how sensitive it with mouse
+    //   float shiftAdd = 250.0f; // faster motion, scaled by duration of shift
+    //   float maxShift = 1000.0f; // Maximum speed when holding shift
+    //   float camSens = 0.25f; // how sensitive it with mouse
     // position in the middle of the screen, rather than at the top
     private Vector3 lastMouse = new Vector3(255, 255, 255); 
     private float totalRun= 1.0f;
@@ -32,8 +31,6 @@ public class camera_fly : MonoBehaviour {
     private float arrowRotateSens = 0.5f;
     private float X;
     private float Y;
-    //! deprecated, see below
-    //! private Vector3 dragOrigin;
     public float sensitivity = 10f;
     private float sphere_scale = 0.35f;
     Vector3 lastMousePosition;
@@ -60,10 +57,6 @@ public class camera_fly : MonoBehaviour {
     //! private float rotationY = 0.0f;
     // trying to add collision
     public Rigidbody rb;
-
-    void Start() {
-        rb = GetComponent<Rigidbody>();
-    }
  
     void pathLink(uint ind0, uint ind1) {
         Debug.Log("[STATUS] computing path");
@@ -78,7 +71,7 @@ public class camera_fly : MonoBehaviour {
             route.Add((int)ec.Current);
             rp1 = hitTransform.TransformPoint(vertices[ec.Current]);
             // dev: the debug lines are not visible for some reason
-            // Debug.DrawLine(rp0, rp1, Color.red, 1000f);
+            //   Debug.DrawLine(rp0, rp1, Color.red, 1000f);
             // increment the previous point
             rp0 = rp1;
             // draw spheres that mark the path
@@ -95,8 +88,6 @@ public class camera_fly : MonoBehaviour {
         {
             check_route += item.ToString() + " ";
         }
-        // render the route for the user
-        Debug.Log(check_route);
     }
 
     void OnGUI() {
@@ -111,32 +102,17 @@ public class camera_fly : MonoBehaviour {
             if (clicked.Count==0) {
                 error = "no selected vertices";
             }
-            //! dev: json ends up blank
-            //! string clicked_s = JsonUtility.ToJson(clicked);
-            //! Debug.Log("[STATUS] serialized: "+clicked_s);
-            //! string path_out = Application.persistentDataPath+"/out.json";
-            //! dev: export only the relevant information: the user-marked spheres since we can
-            //!   easily reproduce Dijkstra in Python to complete the path
-            
-            string clicked_s = "";
-            for (int i=0; i < clicked.Count - 1; i++)
-            {
-                clicked_s += clicked[i].ToString() + ",";
-            }
-            clicked_s += clicked[clicked.Count-1];
-            var path_out = (
-                Regex.Replace(source_path, @"^file://", "") + 
-                    ".tagged." + boundName + ".txt"
-            );
-            System.IO.File.WriteAllText(path_out, clicked_s);
-            
+          
             Landmarks dump = new Landmarks(clicked,route);
             string json = JsonUtility.ToJson(dump);
+            var path_out = (
+                Regex.Replace(source_path, @"^file://", "") + 
+                    ".tagged." + boundName + ".json"
+            );
             System.IO.File.WriteAllText(path_out, json);
             
-            
             Debug.Log("[STATUS] saved");
-            //! this cannot build easily UnityEditor.EditorUtility.RevealInFinder(path_out);
+            // testing only: UnityEditor.EditorUtility.RevealInFinder(path_out);
             // via: https://answers.unity.com/questions/24257/\
             //   how-do-i-find-all-game-objects-with-the-same-name.html
             // change the colors after saving so the spheres remain as a reminder
@@ -178,46 +154,13 @@ public class camera_fly : MonoBehaviour {
         }
         else if(Input.GetMouseButton(0))
         {
-            // other references:
-            //   https://answers.unity.com/questions/1189946/click-and-drag-to-rotate-camera-like-a-pan.html
-            //   https://answers.unity.com/questions/1344322/free-mouse-rotating-camera.html 
-            // camera_fly premise via: 
-            //   https://forum.unity.com/threads/fly-cam-simple-cam-script.67042/
-            // previous method deprecated in favor of quaternions because we cannot go all the way under
-            //   Vector3 delta = Input.mousePosition - lastMousePosition;
-            //   rpb writes novel code pattern matching here
-            //   lastMouse = new Vector3(-delta.y * camSens, delta.x * camSens, 0);
-            //   lastMouse = new Vector3(
-            //       transform.eulerAngles.x + lastMouse.x , 
-            //       transform.eulerAngles.y + lastMouse.y, 
-            //       0);
-            //   transform.eulerAngles = lastMouse;
-            //   lastMousePosition = Input.mousePosition;
-
-            // ...!!! problem: weird rotations
             // via: https://answers.unity.com/questions/918884/using-quaternion-for-mouse-movement.html
             var r = transform.rotation;
             r = Quaternion.AngleAxis(-Input.GetAxis("Mouse X"), transform.up) * r;
             r = Quaternion.AngleAxis(Input.GetAxis("Mouse Y"), transform.right) * r;
             // not sure where the horizontal came from but it messes things up
-            // r = Quaternion.AngleAxis(Input.GetAxis("Horizontal"), transform.forward) * r;
+            //   r = Quaternion.AngleAxis(Input.GetAxis("Horizontal"), transform.forward) * r;
             transform.rotation = r;
-
-            // ...!!! problem: crazy camera spinning
-            // var r = transform.rotation;
-            // Vector3 delta = Input.mousePosition - lastMousePosition;
-            // lastMouse = new Vector3(-delta.y * camSens, delta.x * camSens, 0);
-            // r = Quaternion.AngleAxis(lastMouse.x, transform.up) * r;
-            // r = Quaternion.AngleAxis(-lastMouse.y, transform.right) * r;
-            // transform.rotation = r;
-
-            // ...!!! problem: jittery sometimes and other times crazy camera
-            //! var r = transform.rotation;
-            //! Vector3 delta = Input.mousePosition - lastMousePosition;
-            //! r = Quaternion.AngleAxis(delta.x, transform.up) * r;
-            //! r = Quaternion.AngleAxis(-delta.y, transform.right) * r;
-            //! transform.rotation = r;
-            //! lastMousePosition = Input.mousePosition;
         }
         else if (Input.GetMouseButtonDown(1))
         {
@@ -282,11 +225,12 @@ public class camera_fly : MonoBehaviour {
         Vector3 p = GetBaseInput();
         if (p.sqrMagnitude > 0){ 
             if (Input.GetKey (KeyCode.RightShift)){
-                // totalRun += Time.deltaTime;
-                // p  = p * totalRun * shiftAdd;
-                // p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-                // p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-                // p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+                // previously included some time-scaling
+                //   totalRun += Time.deltaTime;
+                //   p  = p * totalRun * shiftAdd;
+                //   p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+                //   p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+                //   p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
                 totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
                 p = p * altSpeed;
             } else {
@@ -294,69 +238,26 @@ public class camera_fly : MonoBehaviour {
                 p = p * mainSpeed;
             }
             p = p * Time.deltaTime;
-            // intervene to handle collision
+            // dev: attempt to intervene to handle collision
+            // we generally fail to add force to a rigid body and sphere
+            //   collider on the camera to prevent it from clipping
             // prevent stumbling sideways through a wall
             //   this looks foward: Ray ray = new Ray(transform.position, transform.forward);
-            Ray ray = new Ray(transform.position, p);
-            RaycastHit hit;
-            //! if (Physics.Raycast(ray,out hit,1.0f)) 
-            //! {  
-            //!     Debug.Log("got a hit p " + p + " " +hit.distance);
-            //!     p = p - Vector3.Normalize(p) * (10.0f - hit.distance);
-            //!     Debug.Log("new p " + p);
-            //!     // if (Vector3.Magnitude(p)>4.0f) {
-            //!     //     p = Vector3.Normalize(p) * 4.0f;
-            //!     //     Debug.Log("throttled!" + p);
-            //!     // }
-            //! }
-            //! // THIS IS A CRUDE COLLIDER
-            //! if (!Physics.Raycast(ray,out hit,4.0f)) {
-            //!     transform.Translate(p);
-            //! }
-
-            // THIS SORT OF WORKS
-            //! if (!Physics.Raycast(ray,out hit,1.0f)) {
-            //!     transform.Translate(p);
-            //! } else {
-            //!     p = p - Vector3.Normalize(p) * (1.0f - hit.distance);
-            //!     transform.Translate(p);
-            //! }
+            // original collider code is passable but not great so we abandon
+            //   Ray ray = new Ray(transform.position, p);
+            //   RaycastHit hit;
+            //   if (!Physics.Raycast(ray,out hit,1.0f)) {
+            //       transform.Translate(p);
+            //   } else {
+            //       p = p - Vector3.Normalize(p) * (1.0f - hit.distance);
+            //       transform.Translate(p);
+            //   }
             transform.Translate(p);
-            
-            /// trying with force, rigid body, sphere collider on the camera
-            // this really fails
-            //! rb.AddForce(-p.normalized * 5000f * Time.deltaTime);
         }
         // hold the object in the center of the screen to sweep the camera
         if (Input.GetKey (KeyCode.L)){
-            // original method cannot go upside down but works well
-            // issue: when we switched to quaternion rotation above, we have an orientation problem. 
-            //   that is, when we flip the camera upside down, and hit (or hold L), it flips the orientation
-            //! var origin = new Vector3(0,0,0);
-            //! GameObject.Find("camera").transform.LookAt(origin);
-
-            //! ref https://answers.unity.com/questions/46583/how-to-get-the-look-or-forward-vector-of-the-camer.html
-            //! ref https://answers.unity.com/questions/24805/preventing-lookat-from-flipping.html
-            
-            // ...!!! problem: you can only do this sporadically, not continuously
-            //! var origin = new Vector3(0,0,0);
-            //! var relativePos = origin - transform.position;
-            //! Vector3 look = GameObject.Find("camera").transform.TransformDirection(Vector3.forward);
-            //! transform.rotation = Quaternion.LookRotation(relativePos,look);
-
-            //! ...!!! problem: same issue as above
-            // ref: https://forum.unity.com/threads/object-lookat-camera-is-inverted.698786/
-            //! var origin = new Vector3(0,0,0);
-            //! Transform camera_t = GameObject.Find("camera").transform;
-            //! camera_t.rotation = Quaternion.LookRotation(origin-camera_t.position);
-
-            //! ...!!! problem: fails entirely
-            //! var origin = new Vector3(0,0,0);
-            //! Transform camera_t = GameObject.Find("camera").transform;
-            //! Vector3 forward = Vector3.Cross(transform.up, origin-camera_t.position);
-            //! camera_t.rotation = Quaternion.LookRotation(forward, origin);
-
-            // ref: https://answers.unity.com/questions/32067/rotating-an-object-to-face-the-camera-regardless-o.html#
+            // ref: https://answers.unity.com/questions/32067/\
+            //   rotating-an-object-to-face-the-camera-regardless-o.html#
             // this method works because we can use a second argument for LookAt which is really useful
             var origin = new Vector3(0,0,0);
             Transform camera_t = GameObject.Find("camera").transform;
---
+++ README.md
@@ -0,0 +1,80 @@
+# Liminal-Unity
+
+This small program can be used to view and generate landmarks or boundary conditions on complex Wavefront (`obj`) meshes using a freely rotating camera system. 
+
+The program loads preprocessed meshes in realtime and provides an interface for the user to select (landmark or annotate) a series of vertices, and review the shortest path between them, and then write the results to a named (`JSON`) text file which includes the user-selected vertices along with the shortest paths between them. This tool facilitates fast and flexible landmarking on a complex mesh with a video-game style camera and finds applications in 3D modeling projects which require careful inspection of the mesh. 
+
+The software depends on both pre- and post-processing and was designed to serve as a user landmarking tool supported by [ATgizmo](https://gitlab.com/natalia-trayanova/atgizmo). Please note: the author is not a video game developer! This is a very, very crude tool that accomplishes the critical goal of allowing users to flexibly landmark a mesh with a freely translating and rotating camera.
+
+## Requirements
+
+Input Wavefront (`obj`) files should be preprocessed so they are centered at the origin with a maximum extent of 1000 units. This ensures that the camera location motion scales correctly. Preprocessing instructions will be forthcoming.
+
+## Usage
+
+To use this program, start it up and type a path to an `obj` file in the text box in the upper left. Click "LOAD" and wait for it to load. For reference, it can render a mesh with 1M points and 12M cells seamlessly on a 2017 Macbook Pro with 16GB memory and a Radeon Pro 560 4 GB or with somewhat slow responses on a 2014 macbook pro with integrated graphics.
+
+Use the following camera controls to move the camera:
+
+- `wasd` moves the camera left, right, forwards, and backwards
+- `Space` and `LeftShift` move the camera up and down
+- clicking and dragging with the mouse rotates the camera
+- the arrow keys also rotate the camera
+- holding `RightShift` speeds up the camera
+- holding or pressing the `l` (look) key will center the mesh
+
+Note that the "look" (`l`) key is extremely useful for rotating the mesh in the center of the screen.
+
+To mark landmarks, use the right click button. Each time you click a landmark, you will see a magenta sphere. Each time you select a new sphere, the program will highlight the shortest path (via Dijkstra's algorithm). If you want to visualize a loop, hit the `return` key to connect to the start point. You can use the "RESET" button to clear your selections.
+
+When you are ready to save a landmark, enter a name in the the second textbox in the upper left. When you click "SAVE" it will save a JSON file with the extension `.tagged.<name>.json` as the suffix for the `obj` file you loaded. The JSON file includes `clicked` indices which tell you which vertices were selected by the user and `route` which includes the entire route. Postprocessing can always recapitulate the shortest paths if you only want to use the user-selected `clicked` vertices.
+
+## History
+
+The author used the following development procedure to build this program. Note that we make use of the OBJImport module along with a `.NET` Dijkstra calculator from the nuget link below.
+
+~~~
+1. prelim
+	- make a "new 3D core" project in Unity 2021.3.0f1 LTS
+	- go to project settings and make sure "asset serialization" is text and meta files are text
+	- start tracking in git with gitignore from: https://github.com/github/gitignore/blob/main/Unity.gitignore
+	- rename the scene "liminal-viewer"
+	- rename the Main Camera "camera"
+	- remove the directional light
+	- use the menu to go to window > render > lighting and change skybox material to none to eliminate the horizon
+2. get the pathfinder
+	- get dijkstra. visit https://www.nuget.org/packages/Dijkstra.NET/ and download the dijkstra.net.1.2.1.nupkg 
+		change the extension to zip and find the dll
+		copy the dll to the assets folder for example with: cp -av ~/worker/heart/liminal-unity-eaten/Assets/Dijkstra.NET.dll ~/worker/heart/liminal-unity/Assets/
+3. get the camera fly script
+	- copy in the camera_fly.cs file from the previous version of unity with cp -av ~/worker/heart/liminal-unity-eaten/Assets/camera_fly.cs ~/worker/heart/liminal-unity/Assets
+	- add camera_fly.cs as a script to the camera object using "Add Component" in the inspector
+	- remove the audio listener from the camera
+4. add the obj reader
+	- next add the obj reader with window > package manager. select "my assets" on the packages menu in the upper left and then select "Realtime OBJ Importer" and click the "import" button. skip the samples because we want to avoid any accidents with the ".unity" files (one time this overwrote our scene!) and we will bring in our own usage
+	- add an importer object. click the three dots by the liminal-viewer scene in the hierarchy. use the menu to select "GameObject" then "create empty" and rename it "importer"
+	- copy our modified scripts for the OBJ importer into the assets folder:
+		cp -av ~/worker/heart/liminal-unity-eaten/Assets/ObjFromStream.cs ~/worker/heart/liminal-unity-eaten/Assets/ObjFromFile.cs ~/worker/heart/liminal-unity/Assets/
+	- click the importer in the hierarchy, then in the inspector, click "Add Component". search for "ObjFromStream.cs" and add this as a component. also add the "ObjFromFile.cs" since we can switch between the two during testing
+5. get modifications to the OBJLoader
+	- we need to copy this to make a collider
+		cp -av ~/worker/heart/liminal-unity-eaten/Assets/OBJImport/OBJLoader.cs ~/worker/heart/liminal-unity/Assets/OBJImport/OBJLoader.cs
+6. add tags for the spheres and prebuild shaders
+	- use the menu edit > project settings. search "tags" and add tags for "mark_sphere" and "route_sphere"
+	- go to "always included shaders" and add "Standard (Specular setup)"
+7. add a directional light
+	- add a light to the camera called "light" (right click the camera, then find it on the light menu)
+	- set the X to 1000
+8. note that collision was abandoned. sometimes it's nice to clip and see inside
+9. test. run the program with the stream loader pointed to an obj file
+	- in the importer you can select or deselect the ObjFromStream.cs and ObjFromFile.cs objects
+	- you have to wait a moment for it to load
+	- switch back to the file loader and we will try building it
+10. test build.
+	- go to file > build settings. enable development build
+	- then go to "player" and expand "resolution and presentation" and set the resolution to 1024x768 for the 2017 macbook
+	- set fullscreen mode to "windowed" and allow the window resizing
+	- build and run the program. test with an OBJ that has been mean-centered with a maximum XYZ span of 1000 units
+~~~
+
+These notes are retained for posterity, however the author expects that the projected can be rebuild and developed further from the available source code.
